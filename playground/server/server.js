2860f003ed5759027214a95ea4332d326b61ab8f
jon <manojam79@gmail.com>
2018-09-28 22:36:26 +1000

    JSON RESTFUL server

--- playground/server/server.js
+++ playground/server/server.js
@@ -1,54 +1,38 @@
-// //load mongoose
-// var mongoose = require('mongoose');
-
-// //use promises in mongoose instead of callbacks
-// mongoose.Promise = global.Promise;
-
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-// //Create a model
-// var Todo = mongoose.model('Todo', {  //Constructor function
-//     text: {
-//         type: String,
-//         required: true,
-//         minlength: 1,
-//         trim: true
-//     },
-//     completed: {
-//         type: Boolean,
-//         required: true,
-//         default: false
-//     },
-//     completedAt: {
-//         type: Number,
-//         default: null
-//     }
-// });
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./model/todo');
 
-var newTodo = new Todo({
-    text: 'cook dinner',
-    completed: false
+var expressApp = express();
+
+//middleware
+expressApp.use(bodyParser.json());
+
+//Create
+expressApp.post('/todos', (req, resp) => {
+    var newTodo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed
+    });
+    //Save to DB
+    newTodo.save()
+        .then((doc) => {
+            console.log('Saved !', doc);
+
+            resp.send(doc);
+
+            //mongoose.disconnect();  //TODO
+        })
+        .catch(err => {
+            console.log('Error saving', err);
+
+            resp.status(400).send(err);
+        });
+
+
 });
 
-//Save to DB
-
-newTodo.save()
-    .then((doc) => {
-        console.log('Saved !', doc);
-        mongoose.disconnect();  //TODO
-    })
-    .catch(err => {
-        console.log('Error saving', err);
-    });
-
-
-
-
-
-
-
-
-
+expressApp.listen(3000, () => {
+    console.log("Started on port 3000");
+});
